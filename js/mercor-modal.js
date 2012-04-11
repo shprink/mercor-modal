9e1fa1dc72c4d4280be7fcd29d79428c7f5d7b05
Julien Renaux <julien.renaux@gmail.com>
2012-04-11 22:38:59 +0800

    add documentation

--- js/mercor-modal.js
+++ js/mercor-modal.js
@@ -1,3 +1,45 @@
+/*
+---
+
+name: MercorModal
+
+version: 1.0
+
+description: A lightweight Mootools Class that provides modal
+
+authors: Julien Renaux
+
+repository: https://github.com/shprink/mercor-modal
+
+requires: 
+- Mootools Core/Element.Event
+- Mootools More
+
+license: MIT
+
+Copyright (c) 2012 Julien Renaux <contact@julienrenaux.fr>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+...
+*/
+
 var MercorModal = new Class({
 
 	Implements : [Events, Options],
